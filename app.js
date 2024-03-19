c50082326eda00620d75e6b2158d1f9061422b12
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2024-03-19 20:42:27 +0800

    inaayos ang nagulo

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "eshop",
+    dbName: "JsonMobile",
   })
   .then(() => {
     console.log("Database Connection is ready...");

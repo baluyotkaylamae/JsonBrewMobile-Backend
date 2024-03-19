fc16d45bf1762692abee0c7b30cb2b3684c7af8f
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2024-03-19 20:27:18 +0800

    products

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
-// const productsRoutes = require("./routes/products");
+const productsRoutes = require("./routes/products");
 const usersRoutes = require("./routes/users");
 // const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
 app.use(`${api}/categories`, categoriesRoutes);
-// app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 // app.use(`${api}/orders`, ordersRoutes);
 
